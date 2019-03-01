5343497aa0f26f3f0a9093b6c8aeff304fb6c89e
calix <darwin.calix@d2i-solutions.com>
2019-03-01 08:37:16 -0600

    [Odoo 12] Update 3.3 Carpetas

--- vitt_fiscal_seq/models/fiscal_sequence.py
+++ vitt_fiscal_seq/models/fiscal_sequence.py
@@ -29,7 +29,6 @@ class Authorization(models.Model):
         if vals.get("start_date") > vals.get("expiration_date"):
             raise Warning(_('Start date is greater than than expiration date'))
         _obj = self.env["vitt_fiscal_seq.authorization_code"].search([['active', '=', True] ,['code_type', '=', vals.get("code_type")]])
-        # _obj2 = self.env["vitt_fiscal_seq.authorization_code_type"].search([['id', '=', vals.get("code_type")]])
         array  = []
         if vals.get("start_date") > vals.get("expiration_date"):
             raise Warning(_('Start date is greater than than expiration date'))   
@@ -44,8 +43,34 @@ class Authorization(models.Model):
         elif len_cai.isupper() == False:
             raise Warning(_('Formato del CAI debe ser en mayuscula!')) 
         return res
-
+    @api.one
+    def get_from_to(self):
+        for rec in self:
+            for obj in rec.fiscal_sequence_regime_ids:
+                self._from_ = obj._from
+                self._to_ = obj._to
+                
     @api.multi
+    def get_action_journal_settings(self):
+        ctx = {'company_id': self.company_id.id}
+        return {'name': _("Journal Settings and Fiscal Sequences"),
+                'view_mode': 'form',
+                'view_type': 'form',
+                'res_model': 'vitt_fiscal_seq.journal_settings',
+                'type': 'ir.actions.act_window',
+                'nodestroy': True,
+                'target': 'new',
+                'context': ctx}
+    def _update_ir_sequence(self):
+        for fiscal_sequence in self.fiscal_sequence_regime_ids:
+            if fiscal_sequence.sequence_id:
+                sequence_vals = {'expiration_date': self.expiration_date}
+                fiscal_sequence.sequence_id.write(sequence_vals)
+        return True
+    def write(self, vals):
+        res = super(Authorization, self).write(vals)
+        res = self._update_ir_sequence()
+        return res
     def get_regimen(self):
         dicc = { 
         '000-001-01-' : '01 Facturas',
@@ -59,39 +84,6 @@ class Authorization(models.Model):
         for x in dicc.items():
             lst.append(x)
         return lst
-
-    @api.one
-    def get_from_to(self):
-        for rec in self:
-            for obj in rec.fiscal_sequence_regime_ids:
-                self._from_ = obj._from
-                self._to_ = obj._to
-
-    @api.multi
-    def get_action_journal_settings(self):
-        ctx = {'company_id': self.company_id.id}
-        return {'name': _("Journal Settings and Fiscal Sequences"),
-                'view_mode': 'form',
-                'view_type': 'form',
-                'res_model': 'vitt_fiscal_seq.journal_settings',
-                'type': 'ir.actions.act_window',
-                'nodestroy': True,
-                'target': 'new',
-                'context': ctx}
-
-    @api.multi
-    def _update_ir_sequence(self):
-        for fiscal_sequence in self.fiscal_sequence_regime_ids:
-            if fiscal_sequence.sequence_id:
-                sequence_vals = {'expiration_date': self.expiration_date}
-                fiscal_sequence.sequence_id.write(sequence_vals)
-        return True
-
-    @api.multi
-    def write(self, vals):
-        res = super(Authorization, self).write(vals)
-        res = self._update_ir_sequence()
-        return res
 
 
 class Fiscal_sequence(models.Model):
@@ -113,14 +105,6 @@ class Fiscal_sequence(models.Model):
             res = prefix + str(number).zfill(padding)
         return res
 
-    @api.multi
-    def _update_ir_sequence(self):
-        if self.sequence_id:
-            sequence_vals = {'vitt_min_value': self.build_numbers(self._from),
-                             'vitt_max_value': self.build_numbers(self._to),
-                             }
-            self.sequence_id.write(sequence_vals)
-
     @api.onchange("actived")
     def onchange_actived(self):
         if not self.actived:
@@ -133,21 +117,23 @@ class Fiscal_sequence(models.Model):
     def write(self, vals):
         super(Fiscal_sequence, self).write(vals)
         self._update_ir_sequence()
-
-    @api.multi
     def create(self, vals):
         res = super(Fiscal_sequence, self).create(vals)
         if not vals.get("journal_id"):
             raise Warning(_('Set a journal and a sequence'))
         return res
-
     # TODO : Verificar que no exista en facturas esta secuencia
-    @api.multi
     def unlink(self):
         invoice = self.env["account.invoice"].search([('sequence_ids', '=', self.sequence_id.id)])
         if invoice:
             raise Warning(_('You cannot delete a fiscal regime, you must disable it'))
         return super(Fiscal_sequence, self).unlink()
+    def _update_ir_sequence(self):
+        if self.sequence_id:
+            sequence_vals = {'vitt_min_value': self.build_numbers(self._from),
+                             'vitt_max_value': self.build_numbers(self._to),
+                             }
+            self.sequence_id.write(sequence_vals)
 
 
 # class Code_authorization_type(models.Model):
