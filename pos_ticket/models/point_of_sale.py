90eb14168513d359af5282af4a97e12e64ef79d3
calix <darwin.calix@d2i-solutions.com>
2019-03-04 08:29:15 -0600

    [Odoo 12] Pos metodo action_date_assign_pos

--- pos_ticket/models/point_of_sale.py
+++ pos_ticket/models/point_of_sale.py
@@ -6,6 +6,9 @@ class PosOrder(models.Model):
 
     #sar_number = fields.Char(string='Número de Factura', readonly=True, default=False, help="Unique number of the invoice, computed automatically when the invoice is created.", copy=False)
     
+    sequence_ids_pos = fields.Many2one("ir.sequence", "Fiscal Number", states={'draft': [('readonly', False)]},
+                                   domain="[('is_fiscal_sequence', '=',True),('active', '=', True), '|',('code','=', type),('code','=', 'in_refund'),('journal_id', '=', journal_id), '|', ('user_ids','in',False),('user_ids','in', user_id)]")
+
     @api.multi
     def create(self,values):
         new_name = self.env['ir.sequence'].next_by_code('pos_order')
@@ -14,6 +17,17 @@ class PosOrder(models.Model):
         # for pos in self:
         #     pos.write({'name': new_name})
         res = super(PosOrder, self).create(values)
+        return res
+
+    @api.multi
+    def action_date_assign_pos(self):
+        res = super(PosOrder, self).action_date_assign_pos()
+        today = datetime.now().date()
+        if self.sequence_ids_pos:
+            if today > self.sequence_ids_pos.expiration_date:
+                raise Warning(_('The Expiration Date for this fiscal sequence is %s ') % (self.sequence_ids_pos.expiration_date))
+            if self.sequence_ids_pos.vitt_number_next_actual > self.sequence_ids_pos.max_value:
+                raise Warning(_('The range of sequence numbers is finished'))
         return res
 
     # @api.multi
