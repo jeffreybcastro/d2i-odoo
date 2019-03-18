a84ddd3328df744986ea916a8d9d4b7fdcafb4fb
calix <darwin.calix@d2i-solutions.com>
2019-03-18 11:34:38 -0600

    [Odoo 12] Update Fiscal Sequence

--- vitt_fiscal_seq/models/account_invoice.py
+++ vitt_fiscal_seq/models/account_invoice.py
@@ -51,7 +51,6 @@ class AccountInvoice(models.Model):
                     elif line.invoice_line_tax_ids.amount == 00.0000:
                         self.exento =+ line.price_subtotal
     
-                            
 
                     if line.discount:
                         self._desc = self._desc + (  (line.price_unit * line.quantity) - line.price_subtotal )
@@ -67,11 +66,11 @@ class AccountInvoice(models.Model):
                 self.cai_shot = regimen.authorization_code_id.name
 
         for validation in self.sequence_ids:
-            if validation.is_fiscal_sequence:  
+            if validation.type = 'out_invoice':  
                 self.cai_expires_shot = validation.expiration_date
                 self.min_number_shot = str(validation.vitt_min_value)
                 self.max_number_shot = str(validation.vitt_max_value)
-                
+
         return self.write({'state': 'open'})
 
     @api.multi
